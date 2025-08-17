391eb8aa92eb0fe5ce041daf45abcab9d4c1dc35
Andrew T <tamagoch87@icloud.com>
2025-08-17 11:27:23 +0500

    Initial Commit

---
+++ ChartPrototype/ChartPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartPrototypeApp.swift
+//  ChartPrototype
+//
+//  Created by Андрей Цибин on 17.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChartPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartPrototype/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ChartPrototype
+//
+//  Created by Andrew T. on 17.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Its me")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Hello, World!")
+                .padding()
+                .background(Color.green, in: RoundedRectangle(cornerRadius: 8))
+                .foregroundStyle(.white)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
