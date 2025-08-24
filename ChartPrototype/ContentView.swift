1ba68370512b4936c97104a4c685bc950fcd9430
Andrew T <tamagoch87@icloud.com>
2025-08-24 14:13:49 +0500

    Первый коммит: добавил ContentView с примером текста

--- ChartPrototype/ContentView.swift
+++ ChartPrototype/ContentView.swift
@@ -15,8 +15,7 @@ struct ContentView: View {
                 .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
             Text("Hello, World!")
                 .padding()
-                .background(Color.green, in: RoundedRectangle(cornerRadius: 8))
-                .foregroundStyle(.white)
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
